b1f3209e671baaf3e2e58b89017ba21989923649
KarolWojnar <karolwojnar47@gmail.com>
2024-01-15 16:10:35 +0100

    view created

--- app/src/main/java/com/example/gallery/MainActivity.kt
+++ app/src/main/java/com/example/gallery/MainActivity.kt
@@ -1,53 +1,84 @@
 package com.example.gallery
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
+import android.Manifest
+import java.io.File
 
 class MainActivity : ComponentActivity() {
-
-    private val galleryImageView: ImageView by lazy { findViewById<ImageView>(R.id.image_id) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
 
-        val allImagesPaths = getAllImages()
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                1
+            )
+        } else {
 
-        if (allImagesPaths.isNotEmpty()) {
-            galleryImageView.setImageURI(Uri.parse(allImagesPaths[1]))
-        }
-    }
-    private fun getAllImages(): List<String> {
-        val images = mutableListOf<String>()
+            val image_id = findViewById<ImageView>(R.id.image_id)
+            val bPrev_id = findViewById<Button>(R.id.bPrev_id)
+            val bNext_id = findViewById<Button>(R.id.bNext_id)
 
-        val projection = arrayOf(
-            MediaStore.Images.Media._ID,
-            MediaStore.Images.Media.DATA
-        )
+            val allImagesPaths = getAllImages()
 
-        val sortOrder = "${MediaStore.Images.Media.DATE_ADDED} DESC"
+            var currImgIndex = 0
 
-        contentResolver.query(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            projection,
-            null,
-            null,
-            sortOrder
-        )?.use { cursor ->
-            val dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            image_id.setImageURI(Uri.parse(allImagesPaths.get(currImgIndex)))
 
-            while (cursor.moveToNext()) {
-                val data = cursor.getString(dataColumn)
-                images.add(data)
+            bPrev_id.setOnClickListener {
+                image_id.setImageURI(Uri.parse(allImagesPaths.get(currImgIndex - 1)))
+            }
+            bNext_id.setOnClickListener {
+                image_id.setImageURI(Uri.parse(allImagesPaths.get(currImgIndex + 1)))
             }
         }
-        return images
     }
 
+    fun getAllImages():ArrayList<String>{
+        val fileList: ArrayList<String> = ArrayList()
+
+        val path: String = Environment.getExternalStorageDirectory().absolutePath
+
+        File(path).walk().forEach {
+            if (it.toString().endsWith(".jpg"))
+                fileList.add(it.toString())
+        }
+        return fileList
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 1) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val allImagesPaths = getAllImages()
+                // ...
+            } else {
+                // Uprawnienie zostało odrzucone, możesz obsłużyć to zdarzenie odpowiednio
+            }
+        }
+    }
 }
